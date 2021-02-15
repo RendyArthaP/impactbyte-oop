cb3c8c23bd4163a35d4c3c1d67eeca49746e81f7
Rendy Artha <rendyartha312@gmail.com>
2021-02-15 21:54:29 +0700

    setup

---
+++ class/index.js
@@ -0,0 +1,9 @@
+class Enemy {
+  constructor(name, speed, healthPoint, attackPoint, isDead) {
+    this.name = name;
+    this.speed = speed;
+    this.healthPoint = healthPoint;
+    this.attackPoint = attackPoint;
+    this.isDead = isDead;
+  }
+}
